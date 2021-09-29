ab81d423a201f6e41663549ba822de667e196d37
Lowell Perez <70902543+mushamuuu@users.noreply.github.com>
2021-09-29 12:13:35 +0100

    Classes and Objects

---
+++ Media Library/src/Media.java
@@ -0,0 +1,27 @@
+public class Media {
+    String mName;
+    String mDesc;
+
+    public Media(String x, String y){
+        mName = x;
+        mDesc = y;
+    }
+
+    public String getmDesc() {
+        return mDesc;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public void setmDesc(String mDesc) {
+        this.mDesc = mDesc;
+    }
+
+}
+
---
+++ Media Library/src/MediaLibrary.java
@@ -0,0 +1,11 @@
+public class MediaLibrary {
+
+    public MediaLibrary(){
+        Media[] member = new Media[4];
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
