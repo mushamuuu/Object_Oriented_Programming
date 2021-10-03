7016bc90d96ddf7c977283c9de6d59761705690e
Lowell Perez <70902543+mushamuuu@users.noreply.github.com>
2021-10-03 20:19:02 +0100

    3/10/21

--- Dutch National Problem/src/Main.java
+++ Dutch National Problem/src/Main.java
@@ -1,11 +1,10 @@
+import java.util.*;
 /**
- This program generates a random number from 1-3 and append to an array 10 times and display the unsorted and sorted results.
- @author  Lowell Perez
-  * @version 1.0
+ * This program generates a random number from 1-3 and append to an array 10 times and display the unsorted and sorted results.
+ * @author  Lowell Perez
+ * @version 1.0
  * @since   3-10-21
  */
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         RandomNumber r = new RandomNumber();
